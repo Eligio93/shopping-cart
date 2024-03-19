069b512fd9503184cba69ea08611373a69b6abf0
Eligio93 <eligio.cristantielli@gmail.com>
2024-03-19 23:47:00 +1100

    move api call from router to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import Header from "./components/Header";
 import {useLocation, Outlet,Link} from "react-router-dom";
+import { useState,useEffect } from "react";
 import "./index.css";
 
-function App({cart,categories}) {
+function App({cart}) {
   //Check if the page we are at is the home
   const isActive=useLocation().pathname==="/";
   //https://fakeapi.platzi.com/
+  const [categories, setCategories] = useState(null);
+  const [loadingCategories, setLoadingCategories] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getCategories = async function () {
+      try {
+        let response = await fetch(
+          "https://fakestoreapi.com/products/categories"
+        );
+        if (!response.ok) {
+          throw new Error("Error in fetching data");
+        }
+        let result = await response.json();
+        setCategories(result);
+      } catch (error) {
+        setError(error);
+        console.log("Errore nel fetching data")
+        setCategories(null);
+      } finally {
+        setLoadingCategories(false);
+       console.log(categories)
+      }
+    };
+
+    getCategories();
+  }, []);
 
   return (
     <>
--- src/Router.jsx
+++ src/Router.jsx
@@ -6,32 +6,7 @@ import { useEffect, useState } from "react";
 import Categories from "./Categories";
 
 const Router = () => {
-  const [categories, setCategories] = useState(null);
-  const [loadingCategories, setLoadingCategories] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getCategories = async function () {
-      try {
-        let response = await fetch(
-          "https://fakestoreapi.com/products/categories"
-        );
-        if (!response.ok) {
-          throw new Error("Error in fetching data");
-        }
-        let result = await response.json();
-        setCategories(result);
-      } catch (error) {
-        setError(error);
-        console.log("Errore nel fetching data")
-        setCategories(null);
-      } finally {
-        setLoadingCategories(false);
-      }
-    };
-
-    getCategories();
-  }, []);
+  
   const [cart, setCart] = useState([]);
   //add to cart function
   function addToCart(product, quantity) {
@@ -64,7 +39,7 @@ const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App cart={cart} categories={categories} />,
+      element: <App cart={cart} />,
       children: [
         {
           path: "products",
