462f880e85ed08e6d3a39a5593df649985cdf58e
Eligio93 <eligio.cristantielli@gmail.com>
2024-03-19 23:23:30 +1100

    set api call for category and finalize addToCart function

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,23 +2,77 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Products from "./Products";
 import About from "./About";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Categories from "./Categories";
 
 const Router = () => {
-  const[cart,setCart]=useState([])
-  function addToCart(product,quantity){
-    let totPrice=product.price * quantity
-    setCart([...cart,{title:product.title, quantity:quantity, totPrice:totPrice, img:product.image }]);
+  const [categories, setCategories] = useState(null);
+  const [loadingCategories, setLoadingCategories] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const getCategories = async function () {
+      try {
+        let response = await fetch(
+          "https://fakestoreapi.com/products/categories"
+        );
+        if (!response.ok) {
+          throw new Error("Error in fetching data");
+        }
+        let result = await response.json();
+        setCategories(result);
+      } catch (error) {
+        setError(error);
+        console.log("Errore nel fetching data")
+        setCategories(null);
+      } finally {
+        setLoadingCategories(false);
+      }
+    };
+
+    getCategories();
+  }, []);
+  const [cart, setCart] = useState([]);
+  //add to cart function
+  function addToCart(product, quantity) {
+    let totPrice = product.price * quantity;
+    //get the index to check if the element is already in cart
+    let existingItemIndex = cart.findIndex(
+      (item) => item.title == product.title
+    );
+    //if it's already in cart
+    if (existingItemIndex !== -1) {
+      //update array
+      let updatedCart = [...cart];
+      updatedCart[existingItemIndex].quantity += quantity;
+      updatedCart[existingItemIndex].totPrice += totPrice;
+      //set new Cart
+      setCart(updatedCart);
+    } else {
+      //else add a new item in the cart
+      setCart([
+        ...cart,
+        {
+          title: product.title,
+          quantity: quantity,
+          totPrice: totPrice,
+          img: product.image,
+        },
+      ]);
+    }
   }
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App cart={cart}/>,
+      element: <App cart={cart} categories={categories} />,
       children: [
-       {
+        {
           path: "products",
-          element: <Products addToCart={addToCart}/>,
+          element: <Products addToCart={addToCart} />,
+        },
+        {
+          path: "categories",
+          element: <Categories />,
         },
         {
           path: "About",
