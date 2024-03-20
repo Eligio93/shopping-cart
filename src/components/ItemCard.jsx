501cfcb92b5051c1c2a8b993f3819ae82f558b05
Eligio93 <eligio.cristantielli@gmail.com>
2024-03-20 15:10:54 +1100

    change default quantity to 1

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 function ItemCard({ product, addToCart }) {
-  const[quantity,setQuantity]=useState(0);
+  const[quantity,setQuantity]=useState(1);
   const changeQuantity=(event)=>{
     setQuantity(parseInt(event.target.value));
   }
@@ -12,7 +12,7 @@ function ItemCard({ product, addToCart }) {
       <p className="product-title">{product.title}</p>
 
       <p className="product-price">{"$" + product.price}</p>
-      <input type="number" min="0" max="20" value={quantity} onChange={changeQuantity}/>
+      <input type="number" min="1" max="20" value={quantity} onChange={changeQuantity}/>
       {/*al click sara da prendere il valore in input e aggiornare lo state del carrello*/}
       <button onClick={() => addToCart(product, quantity)}>Add to Cart</button>
     </>
