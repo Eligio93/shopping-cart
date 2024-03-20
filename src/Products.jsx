ac8e48d15f3fe48703f334c39adec1dd529400cf
Eligio93 <eligio.cristantielli@gmail.com>
2024-03-20 13:31:09 +1100

    changes the api call to render items by category

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,24 +1,31 @@
 import { useEffect, useState } from "react";
 import ItemCard from "./components/ItemCard";
+import { useParams } from "react-router-dom";
 
-function Products({addToCart}) {
+function Products({ addToCart }) {
   const [products, setProducts] = useState(null);
   const [loadingProducts, setLoadingProducts] = useState(true);
   const [error, setError] = useState(null);
+  const { category } = useParams();
 
   useEffect(() => {
     const getProducts = async () => {
+      let response;
       try {
-        let response = await fetch(
-          "https://fakestoreapi.com/products/"
-        );
+        if (category == undefined) {
+          response = await fetch(`https://fakestoreapi.com/products`);
+        } else {
+          response = await fetch(
+            `https://fakestoreapi.com/products/category/${category}`
+          );
+        }
         if (!response.ok) {
           throw new Error("Error in fetching data");
         }
         let result = await response.json();
         setProducts(result);
-        console.log(products)
       } catch (error) {
+        console.log(error);
         setError(error);
         setProducts(null);
       } finally {
@@ -26,10 +33,9 @@ function Products({addToCart}) {
       }
     };
 
-    getProducts();   
-  }, []);
+    getProducts();
+  }, [category]);
 
-  
   return (
     <>
       {loadingProducts ? (
