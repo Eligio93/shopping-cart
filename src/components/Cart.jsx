d0643e6a58415d7a161b5f1a09b83ef1fa3f35bb
Eligio93 <eligio.cristantielli@gmail.com>
2024-03-23 00:23:16 +1100

    adds max quantity to item in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,27 +1,35 @@
-function Cart({cart,removeFromCart,changeQuantity}){
-
-    return(
-        <>
-       <div className="cart">
-            {cart.map((item)=>
-       <div className="cart-item" key={item.id}>
-        <div className="cart-item-info">
-        <img src={item.img} alt="" />
-        <div className="cart-item-description">
-        <p><b>{item.title}</b></p>
-        <i>Total: $ {item.totPrice}</i>
-           
-        </div>
-       
-        </div>
-        <input type="number" defaultValue={item.quantity} min="1" onChange={(event)=>changeQuantity(cart,item.id,event,item.price)} />
-        <button onClick={()=>removeFromCart(cart,item.id)}>Remove</button>
-       </div>
-
-       )}
-       </div>
-       <button className="checkOut">Checkout</button>   
-       </> 
-    )
+function Cart({ cart, removeFromCart, changeQuantity }) {
+  return (
+    <>
+      <div className="cart">
+        {cart.map((item) => (
+          <div className="cart-item" key={item.id}>
+            <div className="cart-item-info">
+              <img src={item.img} alt="" />
+              <div className="cart-item-description">
+                <p>
+                  <b>{item.title}</b>
+                </p>
+                <i>Total: $ {item.totPrice}</i>
+              </div>
+            </div>
+            <input
+              type="number"
+              value={item.quantity}
+              min="1"
+              max="20"
+              onChange={(event) =>
+                changeQuantity(cart, item.id, event, item.price)
+              }
+            />
+            <button onClick={() => removeFromCart(cart, item.id)}>
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+      <button className="checkOut">Checkout</button>
+    </>
+  );
 }
-export default Cart
+export default Cart;
