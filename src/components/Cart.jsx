f174bfea929d2e54ceab5661f9f8761d561cc2f7
Eligio93 <eligio.cristantielli@gmail.com>
2024-03-22 13:06:12 +1100

    adds remove from cart fucntion

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,21 @@
-function Cart({cart}){
+function Cart({cart,removeFromCart}){
 
     return(
-       <>
-       {cart.map((item)=>
+        <>
+       <div className="cart">
+            {cart.map((item)=>
        <div className="cart-item" key={item.id}>
         <div className="cart-item-description">
         <img src={item.img} alt="" />
         <p>{item.title}</p>
         </div>
         <input type="number" value={item.quantity} min="1" onChange={()=>console.log("ciao")} />
-        <button>Remove</button>
-       
+        <button onClick={()=>removeFromCart(cart,item.id)}>Remove</button>
        </div>
 
        )}
+       </div>
+       <button className="checkOut">Checkout</button>   
        </> 
     )
 }
