4d69b1997393911701f26bf1733bc9a6be83a2fe
Eligio93 <eligio.cristantielli@gmail.com>
2024-03-22 13:05:52 +1100

    init cart' side bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 import cartIcon from "../assets/img/cart-icon.svg";
 import { useEffect, useState } from "react";
-import { NavLink,useLocation} from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import Cart from "./Cart";
 
-
-function Header({ cart, categories }) {
-  const[cartSideBar,setCartSideBar]=useState(false);
-  let location= useLocation();
-  useEffect(()=>{
-    setCartSideBar(false)
-  },[location])
-  function handleCartSideBar(){
-    setCartSideBar(!cartSideBar)
+function Header({ cart, categories, removeFromCart }) {
+  const [cartSideBar, setCartSideBar] = useState(false);
+  let location = useLocation();
+  useEffect(() => {
+    setCartSideBar(false);
+  }, [location]);
+  function handleCartSideBar() {
+    setCartSideBar(!cartSideBar);
   }
+
   return (
     <div className="header">
       <h1>Ec Fake Store</h1>
@@ -41,16 +41,25 @@ function Header({ cart, categories }) {
       </ul>
       <div className="social-bar">
         {/*al click del button si aprira il div laterale*/}
-        <button onClick={()=>{handleCartSideBar();console.log(cart)}}>
+        <button
+          onClick={() => {
+            handleCartSideBar();
+            console.log(cart);
+          }}
+        >
           <img className="cartIcon" src={cartIcon} alt="" />
           {cart.length > 0 && <p className="cart-quantity">{cart.length}</p>}
         </button>
       </div>
-      { cartSideBar &&  <div className="cart-sidebar">
-        {cart.length == 0 ? <p>Aggiungi elementi</p> : <Cart cart={cart} />}
-        
-        </div>}
-     
+      {cartSideBar && (
+        <div className="cart-sidebar">
+          {cart.length == 0 ? (
+            <p>Aggiungi elementi</p>
+          ) : (
+            <Cart cart={cart} removeFromCart={removeFromCart} />
+          )}
+        </div>
+      )}
     </div>
   );
 }
