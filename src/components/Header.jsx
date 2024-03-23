f307fdfa3c9b2ddcfa7a3cab075df2a2de985d62
Eligio93 <eligio.cristantielli@gmail.com>
2024-03-23 14:17:13 +1100

    init hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import cartIcon from "../assets/img/cart-icon.svg";
+import hamburgerIcon from"../assets/img/hamburger-icon.svg"
 import { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import Cart from "./Cart";
 
 function Header({ cart, categories, removeFromCart, changeQuantity }) {
   const [cartSideBar, setCartSideBar] = useState(false);
+  const [hamburgerMenu,setHamburgerMenu] = useState (false)
   let location = useLocation();
   useEffect(() => {
     setCartSideBar(false);
@@ -12,10 +14,16 @@ function Header({ cart, categories, removeFromCart, changeQuantity }) {
   function handleCartSideBar() {
     setCartSideBar(!cartSideBar);
   }
+  function handleHamburgerMenu(){
+    setHamburgerMenu(!hamburgerMenu)
+  }
 
   return (
     <div className="header">
-      <div className="hamburger-menu"></div>
+      <button onClick={handleHamburgerMenu} className="ham-menu">
+      <img className="hamburger-icon" src={hamburgerIcon} alt="" />
+      </button>
+        
       <h1>Ec Fake Store</h1>
 
       <ul className="navigation">
@@ -45,7 +53,6 @@ function Header({ cart, categories, removeFromCart, changeQuantity }) {
         <button
           onClick={() => {
             handleCartSideBar();
-            console.log(cart);
           }}
         >
           <img className="cartIcon" src={cartIcon} alt="" />
@@ -59,6 +66,33 @@ function Header({ cart, categories, removeFromCart, changeQuantity }) {
             removeFromCart={removeFromCart}
             changeQuantity={changeQuantity}
           />
+        </div>
+      )}
+      {hamburgerMenu && (
+        <div className="menu">
+          <ul className="navigation-mobile">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="products">Products</NavLink>
+        </li>
+        <li className="dropdown">
+          <div className="categoriesBtn">Categories</div>
+          <div className="dropdown-links">
+            {categories &&
+              categories.map((category) => (
+                <NavLink key={category} to={"categories/" + category}>
+                  {category}
+                </NavLink>
+              ))}
+          </div>
+        </li>
+        <li>
+          <NavLink to="about">About Us</NavLink>
+        </li>
+      </ul>
+
         </div>
       )}
     </div>
